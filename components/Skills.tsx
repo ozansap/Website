1e781ad46077418c8f4f1032b2cc670d996b804e
Swagnemite <swagnemitee@hotmail.com>
2022-08-27 22:25:54 +0200

    skills levels css

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,6 +3,16 @@ import Link from 'next/link';
 import { FC } from 'react';
 import styles from '../styles/Skills.module.scss';
 
+const skills = [
+  {
+    id: 1,
+    name: "React",
+    img: "",
+    level: "Expert"
+  }
+]
+
+const levels = ["Beginner", "Competent", "Proficient", "Expert"];
 
 type props = {
 
@@ -14,7 +24,28 @@ const Skills: FC<props> = ({
 
   return (
     <div className={styles.Skills}>
-      Skills
+      <div className={styles.List}>
+        
+      </div>
+      <div className={styles.Main}>
+        <div className={styles.Title}>
+          <h2>Skills</h2>
+        </div>
+        
+        <div className={styles.Levels}>
+          {levels.map((l, i) => (
+            <div className={styles.Level} key={i}>
+              <div className={styles.LevelTitle}>
+                {l}
+              </div>
+              <div className={styles.LevelContent}>
+
+              </div>
+            </div>
+          ))}
+        </div>
+        
+      </div>
     </div>
   )
 }
