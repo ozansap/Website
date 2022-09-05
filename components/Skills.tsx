54c748f890224fdf62de6f2102cb790822f42ee9
Swagnemite <swagnemitee@hotmail.com>
2022-09-06 00:55:05 +0200

    added skills icons

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
 import styles from '../styles/Skills.module.scss';
@@ -6,10 +7,100 @@ import styles from '../styles/Skills.module.scss';
 const skills = [
   {
     id: 1,
-    name: "React",
-    img: "",
+    name: "AWS",
+    icon: "aws",
     level: "Expert"
-  }
+  },
+  {
+    id: 2,
+    name: "CSS",
+    icon: "css",
+    level: "Expert"
+  },
+  {
+    id: 3,
+    name: "Docker",
+    icon: "docker",
+    level: "Expert"
+  },
+  {
+    id: 4,
+    name: "Figma",
+    icon: "figma",
+    level: "Expert"
+  },
+  {
+    id: 5,
+    name: "Git",
+    icon: "git",
+    level: "Expert"
+  },
+  {
+    id: 6,
+    name: "Heroku",
+    icon: "heroku",
+    level: "Expert"
+  },
+  {
+    id: 7,
+    name: "HTML",
+    icon: "html",
+    level: "Expert"
+  },
+  {
+    id: 8,
+    name: "JavaScript",
+    icon: "javascript",
+    level: "Expert"
+  },
+  {
+    id: 9,
+    name: "Minecraft",
+    icon: "minecraft",
+    level: "Expert"
+  },
+  {
+    id: 10,
+    name: "MongoDB",
+    icon: "mongodb",
+    level: "Expert"
+  },
+  {
+    id: 11,
+    name: "MySQL",
+    icon: "mysql",
+    level: "Expert"
+  },
+  {
+    id: 12,
+    name: "NextJS",
+    icon: "nextjs",
+    level: "Expert"
+  },
+  {
+    id: 13,
+    name: "NodeJS",
+    icon: "nodejs",
+    level: "Expert"
+  },
+  {
+    id: 14,
+    name: "React",
+    icon: "react",
+    level: "Expert"
+  },
+  {
+    id: 15,
+    name: "TypeScript",
+    icon: "typescript",
+    level: "Expert"
+  },
+  {
+    id: 16,
+    name: "VueJS",
+    icon: "vuejs",
+    level: "Expert"
+  },
 ]
 
 const levels = ["Beginner", "Competent", "Proficient", "Expert"];
@@ -24,14 +115,19 @@ const Skills: FC<props> = ({
 
   return (
     <div className={styles.Skills}>
-      <div className={styles.List}>
-        
-      </div>
-      <div className={styles.Main}>
+      <div className={styles.Header}>
         <div className={styles.Title}>
           <h2>Skills</h2>
         </div>
-        
+        <div className={styles.List}>
+          {skills.map((s, i) => (
+            <div className='center' key={i}>
+              <Image src={`/icons/${s.icon}.png`} layout="fill"/>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.Main}>
         <div className={styles.Levels}>
           {levels.map((l, i) => (
             <div className={styles.Level} id={styles[l]} key={i}>
